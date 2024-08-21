48d656778b609dadae3f47c2306d300994596da8
qasim ali sajjad <125833429+qasim-ali14@users.noreply.github.com>
2024-08-21 15:05:28 -0500

    did practice exercise

---
+++ script.js
@@ -0,0 +1,6 @@
+let con=  document.getElementById('containor');
+console.log(con);
+let conn= document.querySelector("p");
+console.log(conn.textContent);
+conn.textContent='journey is going to be a massive'
+// To add a new paragraph to the container
